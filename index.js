d6238e0684bc5ec58ccbad1038804bd3ae49bdee
adina19 <adina.talaba@yahoo.com>
2022-05-08 19:28:14 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const cors=require('cors');
+
+const app=express();
+app.use(cors());
+
+const port=process.env.PORT || 8080;
+
+app.get('/',(req,res) => {
+    res.send('hello world !')
+});
+
+
+app.listen(port,() => {
+    console.log(`CC app listening on port ${port}`)
+});
+
