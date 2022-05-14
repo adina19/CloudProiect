de1210f47958f0ce9ff640b61392e56b47db6127
adina19 <adina.talaba@yahoo.com>
2022-05-14 18:13:19 +0300

    primul seminar

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors=require('cors');
 const bodyParser=require("body-parser");
 
 const messagesRouter=require('./messagesRouter');
+const utilsRouter=require('./utilsRouter')
 
 
 
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.use("/messages",messagesRouter);
+app.use("/utils",utilsRouter);
 
 const port=process.env.PORT || 8080;
 
---
+++ utilsRouter.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const router = express.Router();
+const {detectLanguage, translateText}=require("./translateFunctions.js");
+const {LANGUAGE_ISO_CODE} = require("./dictionaries.js")
+
+router.get("/detect",async (req,res) => {
+    const {text} =req.body;
+
+    if(!text){
+        return res.status(400).send("Missing Parameters");
+    }
+    const languageDetection = await detectLanguage(text);
+    return res.json({
+        language: languageDetection[0]?.language
+})
+
+});
+
+
+
+router.get("/translate",async (req,res) => {
+    const {text, language} =req.body;
+
+    if(!text || !language){
+        return res.status(400).send("Missing Parameters");
+    }
+
+    if(!LANGUAGE_ISO_CODE[language]){
+        return res.status(400).send("Invalid Language");
+    }
+
+    const translatedText = await translateText(text, LANGUAGE_ISO_CODE[language]);
+    return res.json({
+        translateText: translatedText[0]
+})
+
+});
+
+
+module.exports=router;
