2d8f1be211f7466e3a2ac5460b043e39aba2e144
adina19 <adina.talaba@yahoo.com>
2022-05-14 18:32:11 +0300

    Recognition

--- utilsRouter.js
+++ utilsRouter.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router = express.Router();
 const {detectLanguage, translateText}=require("./translateFunctions.js");
 const {LANGUAGE_ISO_CODE} = require("./dictionaries.js")
+const {detectLabels} =require("./imageRecognitionFunctions.js");
 
 router.get("/detect",async (req,res) => {
     const {text} =req.body;
@@ -35,6 +36,19 @@ router.get("/translate",async (req,res) => {
 })
 
 });
+
+
+
+router.get("/labels", async (req,res) => {
+    const {link} = req.body;
+    if (!link){
+        return res.status(400).send ("Missing parameters");
+    }
+    const labels = await detectLabels(link);
+    return res.json({
+        labels
+    })
+})
 
 
 module.exports=router;
